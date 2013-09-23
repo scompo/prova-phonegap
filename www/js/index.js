0ce64ff3200c7451749e02d055fd63521936607a
Mauro Scomparin <scompo@gmail.com>
2013-09-23 18:25:28 +0200

    added location stuff.

--- www/js/index.js
+++ www/js/index.js
@@ -19,3 +19,26 @@ function onFailImg(message) {
     alert('Failed because: ' + message);
 }
 
+//NOTE: Geolocation stuff.
+
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(onSuccess, onFailGPS);
+}
+
+var onSuccess = function(position) {
+    $("#location-contents").html( 'Latitude: '          + position.coords.latitude          + '\n' +
+                                  'Longitude: '         + position.coords.longitude         + '\n' +
+                                  'Altitude: '          + position.coords.altitude          + '\n' +
+                                  'Accuracy: '          + position.coords.accuracy          + '\n' +
+                                  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                                  'Heading: '           + position.coords.heading           + '\n' +
+                                  'Speed: '             + position.coords.speed             + '\n' +
+                                  'Timestamp: '         + position.timestamp                + '\n');
+};
+
+// onError Callback receives a PositionError object
+//
+function onFailGPS(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
